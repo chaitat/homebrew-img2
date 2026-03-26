ed3e35e71aa05976f52373c139986e7a50b07e8f
chaitat <chaiyod@gmail.com>
2026-03-26 09:46:00 +0700

    Update checksum for v1.0.3

--- Casks/img2.rb
+++ Casks/img2.rb
@@ -1,6 +1,6 @@
 cask "img2" do
   version "1.0.3"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "2a67f9aa148855320c5e8173feea3c1ab71aae6428d99233b51a0cb767240d3d"
 
   url "https://github.com/chaitat/img2/releases/download/v#{version}/img2.dmg"
   name "img2"
