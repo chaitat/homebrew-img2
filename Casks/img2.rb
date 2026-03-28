34ea389cd2bfd05c5a82be2df91eea7b08998e70
chaitat <chaiyod@gmail.com>
2026-03-28 21:34:59 +0700

    Update SHA256 for v1.0.4

--- Casks/img2.rb
+++ Casks/img2.rb
@@ -1,6 +1,6 @@
 cask "img2" do
   version "1.0.4"
-  sha256 "cc2b7fbf86586ebe513225690c26de486c486d224c66e7acdb92662d0c51486b"
+  sha256 "430101078e0be13b493c4d6b9c776e5516aea3d3fd50c7f43339c38757c8b069"
 
   url "https://github.com/chaitat/img2/releases/download/v#{version}/img2.dmg"
   name "img2"
